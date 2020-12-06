2d7069003ffa100f92d7dab32aab8c846f134a3a
stefanlester <38575516+stefanlester@users.noreply.github.com>
2020-12-06 22:22:46 +0100

    Done

--- routes/shop.js
+++ routes/shop.js
@@ -2,14 +2,10 @@ const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../util/path');
-const adminData = require('./admin');
+const productsController = require('../controllers/products');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('shop.js', adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-});
+router.get('/', productsController.getProducts);
 
 module.exports = router;
